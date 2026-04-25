caabecbbb1ab08af7807feb561057331fc8767c9
sharma8437 <nikeshsharma100100@gmail.com>
2026-04-26 01:47:48 +0530

    refactor: fix health check typo and update production static file paths

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,28 +3,23 @@ import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express();
-
 const __dirname = path.resolve();
 
-
 app.get("/health", (req, res) => {
-  res.status(200).json({message: "api is up and runing"});
+  res.status(200).json({ message: "api is up and running" });
 });
 
 app.get("/book", (req, res) => {
-  res.status(200).json({message: "book api"});
+  res.status(200).json({ message: "book api" });
 });
 
-
-//make our app ready for production
-if(ENV.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+    res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  });
 }
-  
 
 app.listen(ENV.PORT, () => {
   console.log(`server is running on port ${ENV.PORT}`);
