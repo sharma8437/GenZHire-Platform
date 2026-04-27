bd73e550d3ffa636d82f33536010d7f3d2543b06
sharma8437 <nikeshsharma100100@gmail.com>
2026-04-28 00:32:36 +0530

    feat: add validation check for DB_URL environment variable in database connection logic

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,6 +3,11 @@ import { ENV } from "../lib/env.js";
 
 export const connectDB = async () => {
   try {
+
+    if(!ENV.DB_URL){
+      throw new Error("Please provide DB_URL in the environment variables");
+    }
+
     const conn = await mongoose.connect(ENV.DB_URL);
 
     console.log("✅ MongoDB connected:", conn.connection.host);
