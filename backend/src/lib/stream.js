63c13e60fb8873d2d5833c5603f5cf730f46cf3e
sharma8437 <nikeshsharma100100@gmail.com>
2026-05-03 01:13:24 +0530

    feat: integrate Stream Chat and sync user lifecycle events via Inngest

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,40 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
+
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret) {
+    console.log(" SteramAPI_KEY and STREAM_API_SECRET are missing ");
+    
+}
+
+export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
+
+export const upsertStreamUser = async (userData)=>{
+
+    try {
+        await chatClient.upsertUser(userData)
+        console.log(`Stream user synced successfully:`, userData);
+        
+    } catch (error) {
+        console.error("Error syncing user to Stream :", error);
+    }
+        
+}
+
+
+export const deleteStreamUser = async (userId)=>{
+
+    try {
+        await chatClient.deleteUsers([userId])
+        console.log(`Stream user deleted successfully:`, userId);
+        
+    } catch (error) {
+        console.error("Error deleting user from Stream :", error);
+    }
+        
+}
+
+
